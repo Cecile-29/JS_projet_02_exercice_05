3c6ac8b7cadf1ed828c4b4a86bb8a27112f1705b
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-06 22:57:27 +0200

    push exercice_05

---
+++ app.js
@@ -0,0 +1,16 @@
+// function changePicture(){
+//   document.getElementById('img1').src="./image1_2.jpg";
+// }
+
+// dans la fonction on passe en paramètre (image) elle s'applique a tous
+// les éléments du document déclenchés au survol de la souris
+// la valeur src permet de traiter la variable de manière générique
+// de cette façon, ici la fonction renvoie une source : qui concatène le chemin local
+// de élément et + son id  et + l'extension qui est _2.jpeg
+function changePicture(image){
+document.getElementById(image).src="./img/" + image + "_2.jpg";
+}
+
+// function changePicture(test){
+//  document.getElementById(test).src= test + '_2.jpg';
+// }
