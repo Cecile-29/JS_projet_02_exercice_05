2f5f62fd60aa85be13cdc1b1dd779b78a977719c
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-13 07:34:45 +0200

    push exercice après correction et commentaires

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
 // la valeur src permet de traiter la variable de manière générique
 // de cette façon, ici la fonction renvoie une source : qui concatène le chemin local
 // de élément et + son id  et + l'extension qui est _2.jpeg
-function changePicture(image){
-document.getElementById(image).src="./img/" + image + "_2.jpg";
+function changePicture(toto){
+document.getElementById(toto).src = "./img/" + toto + "_2.jpg";
 }
 
 // function changePicture(test){
